dd4ed0b8ade194057c54424dce3e1c77ee1bc968
syarif soden <65828433+soden46@users.noreply.github.com>
2021-04-18 22:57:33 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,25 +1,30 @@
 <table border="1">
  <tr>
-  <th>No</th>
-  <th>ID Film</th>
-  <th>Nama Film</th>
-  <th>Tempat</th>
+  <th>Id Gudang</th>
+  <th>Stok</th>
+  <th>Tanggal Datang</th>
+  <th>Jenis Produk</th>
+  <th>Id Pesan</th>
+  <th>Kode Produk</th>
  </tr>
 
 <?php  
  include 'koneksi.php';
  $no_urut = 0;
  $search = $_POST['search'];
- $query = "SELECT * FROM theater_jakarta WHERE nama_film LIKE '%$search%'";
+ $query = "SELECT * FROM gudang '%$search%'";
  $result = mysql_query($query);
  while($data = mysql_fetch_array($result)) {
   $no_urut++;
   echo "<tr>
      <td>$no_urut</td>
-     <td>".$data['id']."</td>
-     <td>".$data['nama_film']."</td>
-     <td>".$data['tempat']."</td>
-  <tr>";
+     <td>".$data['idgudang']."</td>
+     <td>".$data['stok_produk']."</td>
+     <td>".$data['tanggal_datang_bunga']."</td>
+     <td>".$data['jenis_produk']."</td>
+     <td>".$data['idpesan_bunga']."</td>
+     <td>".$data['kode_produk']."</td>
+     <tr>";
  }
 ?>
 </table>
