c3b7c878b713b220462450c5a90e06bb2f2dcd98
syarif soden <65828433+soden46@users.noreply.github.com>
2021-04-18 21:43:47 +0700

    Add files via upload

---
+++ polyfills.ts
@@ -0,0 +1,9 @@
+import 'zone.js/dist/zone';
+import 'hammerjs';
+
+
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
